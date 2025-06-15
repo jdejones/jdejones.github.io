09b4b75e010b73d63d3d23cd1535158a347ff71e
Derek Jones <jdejones1@gmail.com>
2025-06-15 14:20:03 -0500

    Rename 0595-big-countries/0595-big-countries.py to leetcode_solutions/0595-big-countries/0595-big-countries.py

--- 0595-big-countries/0595-big-countries.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    return world[['name', 'population', 'area']].loc[(world['area'] >= 3000000) | (world['population'] >= 25000000)]

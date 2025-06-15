0b8d3ec39556021ff5859b7719ae2570cb0f000b
Derek Jones <jdejones1@gmail.com>
2025-06-15 14:23:29 -0500

    Rename 0620-not-boring-movies/0620-not-boring-movies.py to leetcode_solutions/0620-not-boring-movies/0620-not-boring-movies.py

--- 0620-not-boring-movies/0620-not-boring-movies.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
-    return cinema.loc[(cinema['id'] % 2 == 1) & (cinema['description'] != 'boring')].sort_values('rating', ascending=False)

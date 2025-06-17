7a56b7390f7bd325230fa1abcb7339c7d4dc3322
Derek Jones <jdejones1@gmail.com>
2025-06-17 07:50:57 -0500

    Time: 697 ms (5.34%), Space: 65.7 MB (100%) - LeetHub

---
+++ 3803-find-products-with-valid-serial-numbers/3803-find-products-with-valid-serial-numbers.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def find_valid_serial_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.loc[products.description.str.contains(r'\bSN\d{4}-\d{4}\b', regex=True)].sort_values('product_id')

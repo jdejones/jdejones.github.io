1517cc37bd4ea97425aa5862e8b962dc0fafca30
Derek Jones <jdejones1@gmail.com>
2025-06-17 08:27:23 -0500

    Rename 3063-method-chaining/3063-method-chaining.py to leetcode_solutions/3063-method-chaining/3063-method-chaining.py

--- 3063-method-chaining/3063-method-chaining.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    return animals[['name', 'weight']].loc[animals.weight > 100].sort_values('weight', ascending=False)['name'].to_frame()
